7d4b1eae09a29556752f7c8752aeb67ea894b591
Oleh Zinchenko <olgznch@gmail.com>
2026-01-27 22:03:02 +0200

    Lesson 3(CBV)(2)

--- MyFirstDjangoProject/main/urls.py
+++ MyFirstDjangoProject/main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from main.views import IndexView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index')
 ]
